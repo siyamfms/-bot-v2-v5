cedab4abc2644aa98fa68db54fe1cdedf4600092
siyamfms <siyam356478@gmail.com>
2026-09-16 20:51:06 +0600

    Update 0out.js

--- Script/commands/0out.js
+++ Script/commands/0out.js
@@ -1,15 +1,31 @@
 module.exports.config = {
-    name: "out",
-    version: "1.0.0",
-    hasPermssion: 2,
-    credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
-    description: "",
-    commandCategory: "Admin",
-    usages: "out [id]",
-    cooldowns: 10,
+	name: "out",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
+	description: "𝐍𝐈𝐉𝐇𝐔𝐌-𝐁𝐎𝐓 Group Leave Command",
+	commandCategory: "Admin",
+	usages: "out [id]",
+	cooldowns: 10,
 };
 
 module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
-}
+	const moment = require("moment-timezone");
+	const date = moment.tz("Asia/Dhaka").format("DD/MM/YYYY");
+	const time = moment.tz("Asia/Dhaka").format("hh:mm:ss a");
+
+	const targetThreadID = (!args[0] || isNaN(args[0])) ? event.threadID : args.join(" ");
+
+	const msgText = `» 👑 𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+───────────────
+» 📅 ${date}
+» ⏰ ${time}
+───────────────
+আমি [,] 🤖 𝆠፝𝐍𝐈𝐉𝐇𝐔𝐌-𝐂𝐇𝐀𝐓-𝐁𝐎𝐓 🤖👋 আমাকে ব্যবহার করার জন্য ধন্যবাদ 😘আলবিদা সবাই! আমি এখন গ্রুপ থেকে বের হচ্ছি...😞
+───────────────
+» 🧚‍♀️ ‿𝗡𝗜𝗝𝗛𝗨𝗠 𝗖𝗛𝗔𝗧𝗕𝗢𝗧`;
+
+	return api.sendMessage(msgText, targetThreadID, () => {
+		api.removeUserFromGroup(api.getCurrentUserID(), targetThreadID);
+	});
+};
